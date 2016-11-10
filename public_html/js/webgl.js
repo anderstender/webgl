efef4aabf4c6b46cb0f377ada2659b0d50c1877a
anderstender <andreisamolov@mail.ru>
2016-11-10 21:53:24 +0200

    Добавил возможность работы с цветами

--- public_html/js/webgl.js
+++ public_html/js/webgl.js
@@ -40,119 +40,142 @@
             return null;
         }
         
-        
-        parent.setShader("resources/shaders/fragment/shader-fs.sx", "fragment");
-        parent.setShader("resources/shaders/vertex/shader-vs.sx", "vertex");
-        parent.initShaders();
+        parent.Shader.Init();
 
         parent.gl.clearColor(0.0, 0.0, 0.0, 1.0);
         parent.gl.enable(parent.gl.DEPTH_TEST);
     };
     this.shaderProgram = null;
     this.shadersList = null;
-    this.setShader = function(path, type){
-        if(parent.shadersList === null){
-           parent.shadersList = {};
-        }
-        if(type === false){
-            delete parent.shadersList[path];
-        }else{
-            parent.shadersList[path] = type;
-        }
-    };
-    this.initShaders = function(){
-        if(parent.shadersList === null){//если шейдеры не заданы, то задаем стандартные
-            parent.shadersList = {
-                "resources/shaders/fragment/shader-fs.sx"   : "fragment",
-                "resources/shaders/vertex/shader-vs.sx"     : "vertex"
-            };
-        }
-        parent.shaderProgram = parent.gl.createProgram();
-        for(var path in parent.shadersList){
-            var type = parent.shadersList[path];
-            var shader = parent.getShader(path, type);
-            parent.gl.attachShader(parent.shaderProgram, shader);
-        }
-        parent.gl.linkProgram(parent.shaderProgram);
-        
-        if(!parent.gl.getProgramParameter(parent.shaderProgram, 
-                                            parent.gl.LINK_STATUS)){
-            console.log("Could not initialise shaders");
-        }
-        parent.gl.useProgram(parent.shaderProgram);
-        
-        parent.shaderProgram.vertexPositionAttribute = parent.gl.getAttribLocation(parent.shaderProgram, "aVertexPosition");
-        parent.gl.enableVertexAttribArray(parent.shaderProgram.vertexPositionAttribute);
-        
-        parent.shaderProgram.pMatrixUniform = parent.gl.getUniformLocation(parent.shaderProgram, "uPMatrix");
-        parent.shaderProgram.mvMatrixUniform = parent.gl.getUniformLocation(parent.shaderProgram, "uMVMatrix");      
-    };
-    
-    this.loadShaderCode = function(path){
-        if(typeof path == 'undefined'){
-            return null;
-        }
-        var strCode = null;
-        $.ajax({
-            async : false,
-            url   : path,
-            success : function (data){
-                if(data){
-                    strCode = data;
-                }
+    this.Shader = new (function(){
+        var Shader = this;
+        this.loadCode = function(path){
+            if(typeof path == 'undefined'){
+                return null;
             }
-        });
-        return strCode;
-    }
-    
-    this.getShader = function(path, type){
-        var str = parent.loadShaderCode(path);
-        if(str === null){
-            return null;
-        }
-        var shader;
-        if(type === "fragment"){
-            shader = parent.gl.createShader(parent.gl.FRAGMENT_SHADER);
-        }else if(type === "vertex"){
-            shader = parent.gl.createShader(parent.gl.VERTEX_SHADER);
-        }else{
-            return null;
-        }
+            var strCode = null;
+            $.ajax({
+                async : false,
+                url   : path,
+                success : function (data){
+                    if(data){
+                        strCode = data;
+                    }
+                }
+            });
+            return strCode;
+        };
         
-        parent.gl.shaderSource(shader, str);
-        parent.gl.compileShader(shader);
+        this.Get = function(path, type){
+            var str = parent.Shader.loadCode(path);
+            if(str === null){
+                return null;
+            }
+            var shader;
+            if(type === "fragment"){
+                shader = parent.gl.createShader(parent.gl.FRAGMENT_SHADER);
+            }else if(type === "vertex"){
+                shader = parent.gl.createShader(parent.gl.VERTEX_SHADER);
+            }else{
+                return null;
+            }
+
+            parent.gl.shaderSource(shader, str);
+            parent.gl.compileShader(shader);
+
+            if(!parent.gl.getShaderParameter(shader, parent.gl.COMPILE_STATUS)){
+                console.log(parent.gl.getShaderInfoLog(shader));
+                return null;
+            }
+            return shader;
+        };
         
-        if(!parent.gl.getShaderParameter(shader, parent.gl.COMPILE_STATUS)){
-            console.log(parent.gl.getShadeInfoLog(shader));
-            return null;
-        }
-        return shader;
-    };
+        this.Set = function(path, type){
+            if(parent.shadersList === null){
+                parent.shadersList = {};
+            }
+            if(type === false){
+                delete parent.shadersList[path];
+            }else{
+                parent.shadersList[path] = type;
+            }
+        };
+        
+        
+        this.Init = function(){
+            if(parent.shadersList === null){//если шейдеры не заданы, то задаем стандартные
+                parent.shadersList = {
+                    "resources/shaders/fragment/shader-fs.sx"   : "fragment",
+                    "resources/shaders/vertex/shader-vs.sx"     : "vertex"
+                };
+            }
+            parent.shaderProgram = parent.gl.createProgram();
+            for(var path in parent.shadersList){
+                var type = parent.shadersList[path];
+                var shader = parent.Shader.Get(path, type);
+                parent.gl.attachShader(parent.shaderProgram, shader);
+            }
+            parent.gl.linkProgram(parent.shaderProgram);
+
+            if(!parent.gl.getProgramParameter(parent.shaderProgram, 
+                                                parent.gl.LINK_STATUS)){
+                console.log("Could not initialise shaders");
+            }
+            parent.gl.useProgram(parent.shaderProgram);
+
+            parent.shaderProgram.vertexPositionAttribute = parent.gl.getAttribLocation(parent.shaderProgram, "aVertexPosition");
+            parent.gl.enableVertexAttribArray(parent.shaderProgram.vertexPositionAttribute);
+
+            parent.shaderProgram.vertexColorAttribute = parent.gl.getAttribLocation(parent.shaderProgram, "aVertexColor");
+            parent.gl.enableVertexAttribArray(parent.shaderProgram.vertexColorAttribute);
+
+            parent.shaderProgram.pMatrixUniform = parent.gl.getUniformLocation(parent.shaderProgram, "uPMatrix");
+            parent.shaderProgram.mvMatrixUniform = parent.gl.getUniformLocation(parent.shaderProgram, "uMVMatrix");
+        };
+    });
 
     this.buffers = [];
     
     this.Buffer = new (function(){
-        this.index = 0;
         var Buffer = this;
+        this.index = 0;
         this.Add = function(){
             var index = parent.buffers.length;
-            var buffer = parent.gl.createBuffer();
-            parent.buffers[index] = buffer;
+            var buffer_p = parent.gl.createBuffer();
+            var buffer_c = parent.gl.createBuffer();
+            parent.buffers[index] = {};
+            parent.buffers[index].pos = buffer_p;
+            parent.buffers[index].col = buffer_c;
             Buffer.index = index;
             return index;
         };
         
-        this.SetParams = function(index, itemSize, countItems, type){
+        /*
+         * type = v/c    - vertices/colors
+         */
+        this.SetParams = function(index, itemSize, countItems, gl_type){
             parent.Buffer.SetCurrent(index);
-            parent.buffers[index].itemSize = itemSize;
-            parent.buffers[index].numItems = countItems;
-            parent.buffers[index].type = type;
-            parent.buffers[index].first = 0;
+            parent.buffers[index].pos.itemSize = itemSize;
+            parent.buffers[index].col.itemSize = 4;
+            
+            
+            parent.buffers[index].pos.numItems = countItems;
+            parent.buffers[index].col.numItems = countItems;
+            
+            
+            parent.buffers[index].pos.gl_type = gl_type;
+            parent.buffers[index].col.gl_type = gl_type;
+            
+            parent.buffers[index].pos.first = 0;
+            parent.buffers[index].col.first = 0;
         };
         
-        this.SetCurrent = function(index){
+        /*
+         * bufName = pos/col
+         */
+        this.SetCurrent = function(index, bufName){
             parent.gl.bindBuffer(parent.gl.ARRAY_BUFFER, 
-                                parent.buffers[index]);
+                                parent.buffers[index][bufName]);
             Buffer.index = index;
         };
         
@@ -160,49 +183,68 @@
             return parent.buffers[Buffer.index];
         };
         
-        this.SetData = function(index, vertices){
+        this.SetData = function(index, vertices, colors){
             parent.buffers[index].vertices = vertices;
-            parent.Buffer.SetCurrent(index);
+            parent.buffers[index].colors = colors;
+            
+            parent.Buffer.SetCurrent(index, 'pos');
             parent.gl.bufferData(parent.gl.ARRAY_BUFFER, 
                     new Float32Array(vertices), 
                     parent.gl.STATIC_DRAW);
+                    
+                    
+            parent.Buffer.SetCurrent(index, 'col');
+            parent.gl.bufferData(parent.gl.ARRAY_BUFFER, 
+                    new Float32Array(colors), 
+                    parent.gl.STATIC_DRAW);
+                    
+            
         };
         
         this.SetPosition = function(index, coords){
-            parent.buffers[index].coords = coords;
+            parent.buffers[index].pos.coords = coords;
         };
         
         this.Draw = function(index){
-            parent.Buffer.SetCurrent(index);
+            parent.Buffer.SetCurrent(index, 'pos');
             var curBuffer = parent.Buffer.GetCurrent();
             
-            parent.m4.Translate(curBuffer.coords);
+            parent.m4.Translate(curBuffer.pos.coords);
+            
+            parent.gl.bindBuffer(parent.gl.ARRAY_BUFFER, curBuffer.pos);
             parent.gl.vertexAttribPointer(  parent.shaderProgram.vertexPositionAttribute, 
-                                            curBuffer.itemSize, 
+                                            3, 
                                             parent.gl.FLOAT, 
                                             false, 0, 0);
+                                            
+                                            
+            parent.Buffer.SetCurrent(index, 'col');
+            curBuffer = parent.Buffer.GetCurrent();
+            
+            parent.gl.bindBuffer(parent.gl.ARRAY_BUFFER, curBuffer.col);
+            parent.gl.vertexAttribPointer(  parent.shaderProgram.vertexColorAttribute, 
+                                            4, 
+                                            parent.gl.FLOAT, 
+                                            false, 0, 0);                           
+                                         
             parent.setMatrixUniforms();
-            parent.gl.drawArrays(   curBuffer.type, 
-                                    curBuffer.first, 
-                                    curBuffer.numItems);
+            
+              
+              
+              
               
             //необязательная штука, но так как она возвращает в исходную позицию, то считать становится проще
-            parent.m4.Translate([-curBuffer.coords[0], -curBuffer.coords[1], -curBuffer.coords[2]]); 
+            //parent.m4.Translate([-curBuffer.pos.coords[0], -curBuffer.pos.coords[1], -curBuffer.pos.coords[2]]); 
+            
+            
+            
+            
+            parent.gl.drawArrays(   curBuffer.pos.gl_type, 
+                                    0, 
+                                    curBuffer.pos.numItems);
         };
     });
-    this.initBuffers = function(){
-        var bIndex = parent.Buffer.Add();
-        parent.Buffer.SetParams(bIndex, 3, 3, parent.gl.TRIANGLE_STRIP);
-        
-        parent.Buffer.SetPosition(bIndex, [-1.5, 0.0, -17.0]);
-        
-        var vertices = [
-            0.0,  1.0,  0.0,
-           -1.0, -1.0,  0.0,
-            1.0, -1.0,  0.0
-        ];
-        parent.Buffer.SetData(bIndex, vertices);
-    };
+
     
     this.Items = new (function(){
         /*
@@ -222,12 +264,10 @@
                 parent.Buffer.SetPosition(index, [0.0, 0.0, 0.0]);
             }
             if(typeof params.vertices !== 'undefined' &&
-                    typeof params.itemSize !== 'undefined' &&
-                    typeof params.type !== 'undefined'){
-                var countItem = parseInt(params.vertices.length / params.itemSize);
-                parent.Buffer.SetParams(index, params.itemSize, countItem, params.type);
-                
-                parent.Buffer.SetData(index, params.vertices);
+                    typeof params.gl_type !== 'undefined'){
+                var countItem = parseInt(params.vertices.length / 3);
+                parent.Buffer.SetParams(index, 3, countItem, params.gl_type);
+                parent.Buffer.SetData(index, params.vertices, params.colors);
             }
             return index;
         };
@@ -237,12 +277,11 @@
                 parent.Buffer.SetPosition(index, params.translate);
             }
             if(typeof params.vertices !== 'undefined' &&
-                    typeof params.itemSize !== 'undefined' &&
-                    typeof params.type !== 'undefined'){
-                var countItem = parseInt(params.vertices.length / params.itemSize);
-                parent.Buffer.SetParams(index, params.itemSize, countItem, params.type);
+                    typeof params.gl_type !== 'undefined'){
+                var countItem = parseInt(params.vertices.length / 3);
+                parent.Buffer.SetParams(index, 3, countItem, params.gl_type);
                 
-                parent.Buffer.SetData(index, params.vertices);
+                parent.Buffer.SetData(index, params.vertices, params.colors);
             }
             return index;           
         };
